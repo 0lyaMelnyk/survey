a7c6a5deb20e82f78d632e987f9e455fe9842cf4
Stepan Kurdylo <skurd@softserveinc.com>
2022-05-09 11:41:18 +0300

    Fixed build.

--- VotingSystemBackend/VotingProcess/Queries/InitializeDataQuery.cs
+++ VotingSystemBackend/VotingProcess/Queries/InitializeDataQuery.cs
@@ -1,15 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using VotingSystemBackend.Models;
+﻿using VotingSystemBackend.Models;
 
 namespace VotingProcess.Queries
 {
     internal class InitializeDataQuery
     {
-        public void InitializeDataQuery(string faculty)
+        public InitializeDataQuery(string faculty)
         {
             //var facultyId = gl
             var result = new InitialDataDto();
