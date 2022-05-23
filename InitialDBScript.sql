f7ee75bcf00a37f658f1f6eea0d6cd6fe85857c3
Olya <olga14.olg@gmail.com>
2022-05-23 18:24:40 +0300

    update sql scripts
    -add autoincrement
    -add script for insert data

--- InitialDBScript.sql
+++ InitialDBScript.sql
@@ -1,99 +1,87 @@
-CREATE DATABASE VotingSystemDB;
+--CREATE DATABASE VotingSystemDB;
 --drop database VotingSystemDB 
 --use VotingSystemDB;
 CREATE TABLE Teacher(
-TeacherId  int,
-TeacherName NVARCHAR,
+TeacherId int IDENTITY(1,1) PRIMARY KEY,
+TeacherName VARCHAR(max),
 FacultyTeacherId int,
-SubjectTeacherId int,
-PRIMARY KEY(TeacherId)
+SubjectTeacherId int
 );  
 
 CREATE TABLE Faculty(
-FacultyId int,
-FacultyName NVARCHAR,
-PRIMARY KEY (FacultyId)
+FacultyId int IDENTITY(1,1) PRIMARY KEY,
+FacultyName VARCHAR(max)
 );
 
 CREATE TABLE Subject(
-SubjectId int,
-SubjectName NVARCHAR,
+SubjectId int IDENTITY(1,1) PRIMARY KEY,
+SubjectName VARCHAR(max),
 Semester int,
-Faculty NVARCHAR,
-PRIMARY KEY (SubjectId)
+FacultyId int
 );
 
 CREATE TABLE FacultyTeacher(
-FacultyTeacherId int NOT NULL,
+FacultyTeacherId int NOT NULL IDENTITY(1,1) PRIMARY KEY,
 FacultyId int NOT NULL,
 TeacherId int NOT NULL,
 FOREIGN KEY (TeacherId) REFERENCES Teacher (TeacherId),
-FOREIGN KEY (FacultyId) REFERENCES Faculty (FacultyId),
-PRIMARY KEY(FacultyTeacherId)
+FOREIGN KEY (FacultyId) REFERENCES Faculty (FacultyId)
 );
 
 CREATE TABLE SubjectTeacher(
-SubjectTeacherId int NOT NULL,
+SubjectTeacherId int NOT NULL IDENTITY(1,1) PRIMARY KEY,
 SubjectId int NOT NULL,
 TeacherId int NOT NULL,
 FOREIGN KEY (TeacherId) REFERENCES Teacher (TeacherId),
-FOREIGN KEY (SubjectId) REFERENCES Subject (SubjectId),
-PRIMARY KEY(SubjectTeacherId)
+FOREIGN KEY (SubjectId) REFERENCES Subject (SubjectId)
 );
 
 CREATE TABLE Mark(
-MarkId int,
-MarkDescription NVARCHAR,
-PRIMARY KEY (MarkId)
+MarkId int IDENTITY(1,1) PRIMARY KEY,
+MarkDescription VARCHAR(max)
 );
 
 CREATE TABLE GraduateLevel(
-LevelId int,
-LevelName NVARCHAR,
-PRIMARY KEY (LevelId)
+LevelId int IDENTITY(1,1) PRIMARY KEY,
+LevelName VARCHAR(max)
 );
 
 CREATE TABLE Question(
-QuestionId int,
-QuestionDescription NVARCHAR,
-FormMode int,
-PRIMARY KEY (QuestionId)
+QuestionId int IDENTITY(1,1) PRIMARY KEY,
+QuestionDescription VARCHAR(max),
+FormMode int
 );
 
 CREATE TABLE QuestionMark(
-QuestionMarkId int NOT NULL,
+QuestionMarkId int NOT NULL IDENTITY(1,1) PRIMARY KEY,
 QuestionId int NOT NULL,
 MarkId int NOT NULL,
 FOREIGN KEY (QuestionId) REFERENCES Question(QuestionId),
-FOREIGN KEY (MarkId) REFERENCES Mark(MarkId),
-PRIMARY KEY (QuestionMarkId)
+FOREIGN KEY (MarkId) REFERENCES Mark(MarkId)
 );
 
 CREATE TABLE EducationalProgram(
-EducationalProgramId int,
-EducationalProgramName NVARCHAR,
-PRIMARY KEY(EducationalProgramId)
+EducationalProgramId int IDENTITY(1,1) PRIMARY KEY,
+EducationalProgramName VARCHAR(max)
 );
 
 CREATE TABLE Voter(
-VoterId int NOT NULL,
+VoterId int NOT NULL IDENTITY(1,1) PRIMARY KEY,
 LevelId int, 
 Semester int,
 Course int,
 EducationalProgramId int, 
-PRIMARY KEY(VoterId),
 FOREIGN KEY (LevelId) REFERENCES GraduateLevel(LevelId),
 FOREIGN KEY (EducationalProgramId) REFERENCES EducationalProgram(EducationalProgramId)
 );
 
 CREATE TABLE Vote(
-VoteId int NOT NULL,
+VoteId int NOT NULL IDENTITY(1,1) PRIMARY KEY,
 VoterId int NOT NULL,
 FacultyTeacherId int NOT NULL,
 SubjectTeacherId int NOT NULL,
 QuestionMarkId int NOT NULL,
 VoteDate Date, 
-PRIMARY KEY(VoteId),
 FOREIGN KEY (VoterId) REFERENCES Voter(VoterId),
 FOREIGN KEY (FacultyTeacherId) REFERENCES FacultyTeacher(FacultyTeacherId),
 FOREIGN KEY (SubjectTeacherId) REFERENCES SubjectTeacher(SubjectTeacherId),
